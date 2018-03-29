740ad1ae33494367e50aeab0049949cbab909515
OleksiiMyzgin <mr.oleksii5@gmail.com>
2018-03-29 19:27:03 +0300

    applying redux middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,50 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { createLogger } from 'redux-logger';
 import todoApp from './reducers';
 
-const logger = (store) => (next) => {
-  if (!console.group) {
-    return next;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state', 'color: grey', store.getState());
-    console.log('%c action', 'color: blue', action);
-    const returnValue = next(action);
-    console.log('%c next state', 'color: green', store.getState());
-    console.groupEnd(action.type);
-    return returnValue;
-  };
-};
-
-const promise = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(next);
-  }
-  return next(action);
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware =>
-    store.dispatch = middleware(store)(store.dispatch)
-  );
-};
 
 const configureStore = () => {
-  /* eslint-disable no-underscore-dangle */
-  const store = createStore(
-    todoApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-  /* eslint-enable */
   const middlewares = [promise];
-
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares);
-
-  return store;
+  /* eslint-disable no-underscore-dangle */
+  return createStore(
+    todoApp,
+    applyMiddleware(...middlewares),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  /* eslint-enable */
 };
 
 export default configureStore;
