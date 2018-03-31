ee8fa83a740247fc0e3ca0d174fc41c944c5babd
OleksiiMyzgin <mr.oleksii5@gmail.com>
2018-03-31 11:04:32 +0300

    normalize api response with normalizr

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import { getIsFetching } from '../reducers';
 import * as api from '../api';
 
@@ -14,7 +17,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
     response => {
       dispatch({
         type: 'FETCH_TODOS_SUCCESS',
-        response,
+        response: normalize(response, schema.arrayOfTodos),
         filter,
       });
     },
@@ -32,7 +35,7 @@ export const addTodo = (text) => (dispatch) =>
   api.addTodo(text).then(response => {
     dispatch({
       type: 'ADD_TODO_SUCCESS',
-      response,
+      response: normalize(response, schema.todo),
     });
   });
 
